e312315e0d9ce86dc6a14ee1cb3d35a2449c1f79
Abdoul Dgit config --global user.email abdoulson02@gmail.com <abdoulson02@gmail.com>
2021-01-27 15:03:24 -0500

    added for loop property

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
-console.log("Hello World!\n==========\n");
+
+for ( let i = 1; i < 101; i++) {
+    if ( i % 2 !== 0) 
+console.log("Hello World!", i)
+else if ( i % 2 == 0) {continue};
+
+// It coould also be console.log("Non odd") to console out the string sayin Non odd
+}
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
