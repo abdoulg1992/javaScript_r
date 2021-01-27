c4cb129fbfe69bd60f3721b0392f05fbef5f4f09
Abdoul Dgit config --global user.email abdoulson02@gmail.com <abdoulson02@gmail.com>
2021-01-27 12:37:10 -0500

    tried outmy javaScript if else statement

---
+++ app.js
@@ -0,0 +1,19 @@
+/*let randNum = math.floor */
+
+
+
+let rand = 7;
+let Input = Number (prompt("Input a number Please"));
+let textContent = alert("too low") ;
+let textContent2 = alert("too high") ;
+let textContent3 = alert("Congratulation") ;
+
+    if (Input < rand){
+    console.log(textContent);
+}
+    else if (Input > rand) {
+        console.log(textContent2)
+}
+    else if (Input == rand){
+        console.log(textContent3)
+    }; 
